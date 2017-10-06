bebcd58d31f57709f75652313204beaebc5da28d
John Fritz <johnfritz@Johns-Mac-mini.local>
2017-10-05 17:08:04 -0700

    insert intercom component for chat

--- src/MainPage.jsx
+++ src/MainPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Launcher } from 'react-chat-window';
 import TopCarousel from './TopCarousel';
 import ServicesThumbContainer from './ServicesThumbContainer';
 import AboutUs from './AboutUs';
@@ -9,8 +10,7 @@ import TextDivider from './TextDivider';
 import GetQuote from './GetQuote';
 import PhotoGallery from './PhotoGallery';
 import TopBar from './TopBar';
-import LaunchChatButton from './LaunchChatButton';
-
+//import LaunchChatButton from './LaunchChatButton';
 
 const StyledMainPage = styled.section`
   position: relative;
@@ -22,14 +22,21 @@ class MainPage extends React.Component {
     super(props);
     this.state = {
       modalIsOpen: false,
+      messageList: [],
+      newMessagesCount: 0,
+      isOpen: false,
     };
     this.openChat = this.openChat.bind(this);
     this.closeChat = this.closeChat.bind(this);
+    this._onMessageWasSent = this._onMessageWasSent.bind(this);
+    this._sendMessage = this._sendMessage.bind(this);
+    this.handleClick = this._handleClick.bind(this);
   }
 
   openChat() {
+    console.log(this.state)
     this.setState({
-      modalIsOpen: true,
+      isOpen: true,
     });
   }
 
@@ -38,7 +45,34 @@ class MainPage extends React.Component {
       modalIsOpen: false,
     });
   }
+  _onMessageWasSent(message) {
+    console.log(message)
+    this.setState({
+      messageList: [...this.state.messageList, message]
+    })
+  }
 
+  _sendMessage(text) {
+    console.log('SENT', text)
+    if (text.length > 0) {
+      const newMessagesCount = this.state.isOpen ? this.state.newMessagesCount : this.state.newMessagesCount + 1
+      this.setState({
+        newMessagesCount: newMessagesCount,
+        messageList: [...this.state.messageList, {
+          author: 'them',
+          type: 'text',
+          data: { text }
+        }]
+      })
+    }
+  }
+
+  _handleClick() {
+    this.setState({
+      isOpen: !this.state.isOpen,
+      newMessagesCount: 0,
+    });
+  }
   render() {
     return (
       <StyledMainPage>
@@ -54,10 +88,30 @@ class MainPage extends React.Component {
         <div id="Photos"><PhotoGallery /></div>
         <div id="Testimonials"><Testimonials /></div>
         <div id="ServiceArea"><Contact openChat={this.openChat} /></div>
-        <LaunchChatButton openChat={this.openChat} />
+
+        <Launcher
+          style={{
+            position: 'absolute',
+          }}
+          agentProfile={{
+            teamName: 'Get an estiamte!',
+            imageUrl: 'https://s3.ca-central-1.amazonaws.com/tlpm/pictures/imageedit_1_3880336731.png'
+          }}
+          onMessageWasSent={this._onMessageWasSent}
+          messageList={this.state.messageList}
+          newMessagesCount={this.state.newMessagesCount}
+          handleClick={this._handleClick}
+          isOpen={this.state.isOpen}
+        />
       </StyledMainPage>
     );
   }
 }
 
 export default MainPage;
+
+
+/*
+
+ <LaunchChatButton openChat={this.openChat} />
+*/
