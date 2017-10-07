7e391de554ac35fc04e1add379f738494b1ef612
John Fritz <johnfritz@Johns-Mac-mini.local>
2017-10-07 10:49:37 -0700

    fetch photos from api

--- src/PhotoGallery.jsx
+++ src/PhotoGallery.jsx
@@ -12,7 +12,6 @@ const StyledGallery = styled.section`
   padding-top: 5px;
 `;
 
-
 class PhotoGallery extends React.Component {
   constructor(props) {
     super(props);
@@ -31,52 +30,28 @@ class PhotoGallery extends React.Component {
   }
 
   componentWillMount() {
-    // get list of photos
-    /*
-    Axios.get('/user', {
-      params: {
-        ID: 12345,
-      },
-    })
+    fetch('https://tlpm.ca/webimages')
       .then((response) => {
-        console.log(response);
+        return response.json();
       })
-      .catch((error) => {
-        console.log(error);
-      });
-    */
-    const photos = ['/images/gallery/gallery-sample-1.jpg',
-      '/images/gallery/gallery-sample-2.jpg',
-      '/images/gallery/gallery-sample-3.jpg',
-      '/images/gallery/gallery-sample-4.jpg',
-      '/images/gallery/gallery-sample-5.jpg',
-      '/images/gallery/gallery-sample-6.jpg',
-      '/images/gallery/gallery-sample-7.jpg',
-      '/images/gallery/gallery-sample-8.jpg'];
-    this.numberTotal = photos.length;
+      .then((photos) => {
+        this.setState({
+          photos,
+        });
 
-    this.setState({
-      photos,
-    });
+        this.numberTotal = photos.length;
+      });
   }
 
   getPhotos(baseNum) {
     // the default # of items to show is dependent on the MediaQuery size
-    return (
-      this.state.photos.map((photo, i) => {
-        let val = (
-          <GalleryImage
-            key={i}
-            photo={photo}
-            openModal={this.openModal}
-          />
-        );
-        if (i >= this.state.numberToShow + baseNum) {
-          val = null;
-        }
-        return val;
-      })
-    );
+    return this.state.photos.map((photo, i) => {
+      let val = <GalleryImage key={i} photo={photo.url} openModal={this.openModal} />;
+      if (i >= this.state.numberToShow + baseNum) {
+        val = null;
+      }
+      return val;
+    });
   }
 
   getDefaultNum(screenSize) {
@@ -102,38 +77,41 @@ class PhotoGallery extends React.Component {
   render() {
     return (
       <StyledGallery>
-        {this.state.modalIsOpen &&
-          <GalleryDetail selectedPhoto={this.state.selectedPhoto} photos={this.state.photos} closeModal={this.closeModal} />
-        }
+        {this.state.modalIsOpen && (
+          <GalleryDetail
+            selectedPhoto={this.state.selectedPhoto}
+            photos={this.state.photos}
+            closeModal={this.closeModal}
+          />
+        )}
         <h2>Photo Gallery</h2>
         <ResponsiveMasonry columnsCountBreakPoints={{ 200: 2, 450: 3, 700: 4, 1000: 5 }}>
           <MediaQuery minWidth={1} maxWidth={449}>
-            <Masonry gutter={'5px'}>
-              {this.getPhotos(this.getDefaultNum('XS'))}
-            </Masonry>
+            <Masonry gutter={'5px'}>{this.getPhotos(this.getDefaultNum('XS'))}</Masonry>
           </MediaQuery>
           <MediaQuery minWidth={450} maxWidth={699}>
-            <Masonry gutter={'5px'}>
-              {this.getPhotos(this.getDefaultNum('S'))}
-            </Masonry>
+            <Masonry gutter={'5px'}>{this.getPhotos(this.getDefaultNum('S'))}</Masonry>
           </MediaQuery>
           <MediaQuery minWidth={700} maxWidth={999}>
-            <Masonry gutter={'5px'}>
-              {this.getPhotos(this.getDefaultNum('M'))}
-            </Masonry>
+            <Masonry gutter={'5px'}>{this.getPhotos(this.getDefaultNum('M'))}</Masonry>
           </MediaQuery>
           <MediaQuery minWidth={1000}>
-            <Masonry gutter={'5px'}>
-              {this.getPhotos(this.getDefaultNum('L'))}
-            </Masonry>
+            <Masonry gutter={'5px'}>{this.getPhotos(this.getDefaultNum('L'))}</Masonry>
           </MediaQuery>
         </ResponsiveMasonry>
-        {this.state.numberToShow < this.numberTotal &&
+        {this.state.numberToShow < this.numberTotal && (
           <div>
             <h3>. . .</h3>
-            <FlatButton labelStyle={{ fontSize: '1.0em', padding: '5px 10px' }} label="See more photos" primary onClick={() => { this.setState({ numberToShow: this.state.numberToShow + 5 }); }} />
+            <FlatButton
+              labelStyle={{ fontSize: '1.0em', padding: '5px 10px' }}
+              label="See more photos"
+              primary
+              onClick={() => {
+                this.setState({ numberToShow: this.state.numberToShow + 5 });
+              }}
+            />
           </div>
-        }
+        )}
       </StyledGallery>
     );
   }
